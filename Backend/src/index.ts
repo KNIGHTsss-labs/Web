55162ad4aabf6a0466068b119e0808bea3f6009e
H.M. <jame.jame9424@gmail.com>
2026-05-10 21:13:03 +0700

    value default listen

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -256,6 +256,6 @@ app.post('/login', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`🚀 Server is running at http://localhost:${port}`);
 });
